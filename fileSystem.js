74e134b904225ae855eaab0c04b094bf46d83370
Daniel Moreno <daniel_9127@hotmail.com>
2024-01-01 18:07:17 -0500

    se adjunto package.json para usar modulos de filesystem

--- fileSystem.js
+++ fileSystem.js
@@ -1,1 +1,22 @@
-const fs = require('fs');
+//const fs = require('fs');
+//import {promises as fs} from 'fs'; si lo hacemos asi debemos quitar promises de las peticiones
+import fs from 'fs';
+
+const producto = async () => {
+  await fs.promises.writeFile(
+    './productos.txt',
+    'Hola esta es la lista de productos'
+  );
+
+  await fs.promises.appendFile(
+    './productos.txt',
+    '\nAcá vas a poder observar con que elementos contamos y sus caracteristicas'
+  );
+
+  let respuesta = await fs.promises.readFile('./productos.txt', 'utf-8');
+  console.log(respuesta);
+
+  await fs.promises.unlink('./productos.txt');
+};
+
+producto();
